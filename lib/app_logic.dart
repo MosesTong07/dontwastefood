7d5fb23f21931a83b56200f3347850da5aef64c0
mt8g23 <mt8g23@soton.ac.uk>
2025-10-04 19:41:36 +0100

    Added "Add" button in Fridge

---
+++ lib/app_logic.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppLogic extends ChangeNotifier{
+  var fridgeList = [];
+  
+  //Initialize the fridge pull from database
+  void initializeFridge() {
+
+  }
+}
