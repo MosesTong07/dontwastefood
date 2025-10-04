2d39f6d5cac75e522bab6ea8749b5004281c81eb
Kaguya <cybc1g23@soton.ac.uk>
2025-10-04 23:54:17 +0100

    link ingredients page

--- lib/database.dart
+++ lib/database.dart
@@ -3,31 +3,31 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 final supabase = Supabase.instance.client;
 
 class Database {
-  
   // These table names need to be better
   // They are so bad
-  // IDK if this works 
+  // IDK if this works
 
   // Table of all contents. Start of with all the ingredients in recipes first. Can add more when people add to fridge
   final contentTable = supabase
-    .from('ContentTable')
-    .insert(supabase
-      .from('RecipeSample')
-      .select('ingredients'));
-      // Need to concat list of ingredients and then lowercase and then get unique entries before inserting
+      .from('ContentTable')
+      .insert(supabase.from('RecipeSample').select('ingredients'));
+  // Need to concat list of ingredients and then lowercase and then get unique entries before inserting
 
   final recipesTable = supabase
-    .from('RecipesTable')
-    .insert(supabase
-      .from('RecipeSample')
-      .select('''
+      .from('RecipesTable')
+      .insert(
+        supabase.from('RecipeSample').select('''
             name 
             steps
-            '''));
+            '''),
+      );
 
   final recipeToContentTable = supabase
-    .from('RecipeToContent')
-    .insert(supabase
-      .from('RecipeSample')
-      .select('ingredients'));
+      .from('RecipeToContent')
+      .insert(supabase.from('RecipeSample').select('ingredients'));
+
+  final streamForFridge = supabase
+      .from('FridgeContent')
+      .stream(primaryKey: ['fridge_content_id'])
+      .eq('fridge_id', 1);
 }
--- lib/pages/ingredients.dart
+++ lib/pages/ingredients.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
 import 'package:dontwastefood/ingredient_card.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:dontwastefood/database.dart';
+
+final supabase = Supabase.instance.client;
 
 class IngredientsScreen extends StatefulWidget {
   const IngredientsScreen({super.key});
@@ -77,47 +81,180 @@ class IngredientsScreenContent extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: const Color(0xFFF9F6E7),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
+      body: StreamBuilder(
+        stream: Database().streamForFridge,
+        builder: (content, snapshot) {
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          final data = snapshot.data!;
+          if (data.isEmpty) {
+            return const Text("No data");
+          }
+          //return Text(data.toString());
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  width: double.infinity,
-                  height: headerHeight,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 10, 155, 71),
-                    borderRadius:
-                        BorderRadius.only(bottomRight: Radius.circular(30.0)),
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 20.0, top: 90, right: 20.0),
-                  child: Row(
-                    children: [
-                      FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          'Fridge',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: size.width > 500
-                                ? getAdaptiveFontSize(24)
-                                : getAdaptiveFontSize(28),
-                            fontWeight: FontWeight.bold,
-                          ),
+                Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: headerHeight,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 10, 155, 71),
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(30.0),
                         ),
                       ),
-                      Spacer(),
-                      IconButton(
-                          color: const Color(0xFFF9F6E7),
-                          onPressed: () {
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 20.0,
+                        top: 90,
+                        right: 20.0,
+                      ),
+                      child: Row(
+                        children: [
+                          FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: Text(
+                              'Fridge',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: size.width > 500
+                                    ? getAdaptiveFontSize(24)
+                                    : getAdaptiveFontSize(28),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                          IconButton(
+                            color: const Color(0xFFF9F6E7),
+                            onPressed: () {
+                              showModalBottomSheet<dynamic>(
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Container(
+                                    decoration: const BoxDecoration(
+                                      color: Color(0xFFF9F6E7),
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(30.0),
+                                        topLeft: Radius.circular(30),
+                                      ),
+                                    ),
+                                    height:
+                                        MediaQuery.of(context).size.height *
+                                        0.5,
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        left: 30.0,
+                                        top: 30,
+                                        right: 30,
+                                      ),
+                                      child: SingleChildScrollView(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'Ingredient Name',
+                                              style: TextStyle(
+                                                fontSize: getAdaptiveFontSize(
+                                                  20,
+                                                ),
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            TextField(
+                                              decoration: InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                hintText: 'Enter a name',
+                                              ),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            Text(
+                                              'Quantity (grams)',
+                                              style: TextStyle(
+                                                fontSize: getAdaptiveFontSize(
+                                                  20,
+                                                ),
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            TextField(
+                                              decoration: InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                hintText: 'Enter a value',
+                                              ),
+                                            ),
+                                            SizedBox(height: 10),
+                                            Center(
+                                              child: SizedBox(
+                                                width:
+                                                    200, // adjust this value as needed
+                                                child: ElevatedButton(
+                                                  style:
+                                                      ElevatedButton.styleFrom(
+                                                        backgroundColor:
+                                                            const Color.fromARGB(
+                                                              255,
+                                                              7,
+                                                              131,
+                                                              9,
+                                                            ),
+                                                        foregroundColor:
+                                                            Colors.white,
+                                                      ),
+                                                  child: const Text('Save'),
+                                                  onPressed: () {
+                                                    //TODO: ADD LOGIC TO ADD TO INGREDIENT LIST
+                                                    Navigator.pop(context);
+                                                  },
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+
+                              //TODO: Link to app_logic.dart
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                // Add more recipe content below
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SizedBox(
+                    height: 800,
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      itemCount: data
+                          .length, //TODO: CHANGE THIS ACCORDING TO NUMBER OF AVAILABLE RECIPES
+                      itemBuilder: (context, index) {
+                        final item = data[index];
+                        return InkWell(
+                          onTap: () {
                             showModalBottomSheet<dynamic>(
                               isScrollControlled: true,
                               context: context,
                               builder: (BuildContext context) {
+                                //TODO: CHANGE THIS TO ACTUAL FRIDGE ITEMS
+
                                 return Container(
                                   decoration: const BoxDecoration(
                                     color: Color(0xFFF9F6E7),
@@ -127,112 +264,13 @@ class IngredientsScreenContent extends StatelessWidget {
                                     ),
                                   ),
                                   height:
-                                      MediaQuery.of(context).size.height * 0.5,
+                                      MediaQuery.of(context).size.height * 0.8,
                                   child: Padding(
                                     padding: const EdgeInsets.only(
-                                        left: 30.0, top: 30, right: 30),
-                                    child: SingleChildScrollView(
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            'Ingredient Name',
-                                            style: TextStyle(
-                                              fontSize: getAdaptiveFontSize(20),
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 10),
-                                          TextField(
-                                            decoration: InputDecoration(
-                                              border: OutlineInputBorder(),
-                                              hintText: 'Enter a name',
-                                            ),
-                                          ),
-                                          const SizedBox(height: 10),
-                                          Text(
-                                            'Quantity (grams)',
-                                            style: TextStyle(
-                                              fontSize: getAdaptiveFontSize(20),
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 10),
-                                          TextField(
-                                            decoration: InputDecoration(
-                                              border: OutlineInputBorder(),
-                                              hintText: 'Enter a value',
-                                            ),
-                                          ),
-                                          SizedBox(height:10),
-                                          Center(
-                                            child: SizedBox(
-                                              width:
-                                                  200, // adjust this value as needed
-                                              child: ElevatedButton(
-                                                style: ElevatedButton.styleFrom(
-                                                  backgroundColor:
-                                                      const Color.fromARGB(
-                                                          255, 7, 131, 9),
-                                                  foregroundColor: Colors.white,
-                                                ),
-                                                child: const Text('Save'),
-                                                onPressed: () {
-                                                  //TODO: ADD LOGIC TO ADD TO INGREDIENT LIST
-                                                  Navigator.pop(context);
-                                                },
-                                              ),
-                                            ),
-                                          )
-                                        ],
-                                      ),
+                                      left: 30.0,
+                                      top: 30,
+                                      right: 30,
                                     ),
-                                  ),
-                                );
-                              },
-                            );
-
-                            //TODO: Link to app_logic.dart
-                          },
-                          icon: Icon(Icons.add))
-                    ],
-                  ),
-                )
-              ],
-            ),
-            // Add more recipe content below
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child:SizedBox(
-                    height: 800,
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      itemCount:
-                          10, //TODO: CHANGE THIS ACCORDING TO NUMBER OF AVAILABLE RECIPES
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            showModalBottomSheet<dynamic>(
-                              isScrollControlled: true,
-                              context: context,
-                              builder: (BuildContext context) {
-                                 //TODO: CHANGE THIS TO ACTUAL FRIDGE ITEMS
-  
-                                return Container(
-                                  decoration: const BoxDecoration(
-                                    color: Color(0xFFF9F6E7),
-                                    borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(30.0),
-                                      topLeft: Radius.circular(30),
-                                    ),
-                                  ),
-                                  height: MediaQuery.of(context).size.height *
-                                      0.8, 
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 30.0, top: 30, right: 30),
                                     child: SingleChildScrollView(
                                       child: Column(
                                         crossAxisAlignment:
@@ -265,7 +303,7 @@ class IngredientsScreenContent extends StatelessWidget {
                           borderRadius: BorderRadius.circular(16),
                           child: Container(
                             width: 200,
-                            height:100,
+                            height: 100,
                             margin: const EdgeInsets.only(bottom: 8),
                             decoration: BoxDecoration(
                               color: Colors.orangeAccent,
@@ -280,24 +318,30 @@ class IngredientsScreenContent extends StatelessWidget {
                               ],
                             ),
                             child: Padding(
-                              padding: const EdgeInsets.only(left: 10.0, right: 20.0),
+                              padding: const EdgeInsets.only(
+                                left: 10.0,
+                                right: 20.0,
+                              ),
                               child: Row(
                                 children: [
-                                  Text("Banana"),
+                                  Text(item['ingredients']),
                                   Spacer(),
-                                  Text("100g")
-                                ]
+                                  Text(
+                                    item['quantity'].toString() + item['unit'],
+                                  ),
+                                ],
                               ),
                             ),
                           ),
                         );
-                      
                       },
                     ),
                   ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
