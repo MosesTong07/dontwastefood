1ce0c7a51220620b7afa474df9a344bebd350038
Joshua Pang <whp1g23@soton.ac.uk>
2025-10-04 22:26:28 +0100

    Database start

---
+++ lib/database.dart
@@ -0,0 +1,33 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final supabase = Supabase.instance.client;
+
+class Database {
+  
+  // These table names need to be better
+  // They are so bad
+  // IDK if this works 
+
+  // Table of all contents. Start of with all the ingredients in recipes first. Can add more when people add to fridge
+  final contentTable = supabase
+    .from('ContentTable')
+    .insert(supabase
+      .from('RecipeSample')
+      .select('ingredients'));
+      // Need to concat list of ingredients and then lowercase and then get unique entries before inserting
+
+  final recipesTable = supabase
+    .from('RecipesTable')
+    .insert(supabase
+      .from('RecipeSample')
+      .select('''
+            name 
+            steps
+            '''));
+
+  final recipeToContentTable = supabase
+    .from('RecipeToContent')
+    .insert(supabase
+      .from('RecipeSample')
+      .select('ingredients'));
+}
